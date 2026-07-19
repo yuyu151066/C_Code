ac46782dbb1d67f2c657dd5de7ffa152d6d1e197
yuyu <jgy20070808@hotmail.com>
2026-07-19 22:54:53 +0800

    练习

--- VS_C_Test/01_基础语法/test01.c
+++ VS_C_Test/01_基础语法/test01.c
@@ -2,6 +2,19 @@
 
 int main()
 {
-	printf("yuyu");
+	//printf("     **     \n");
+	//printf("     **     \n");
+	//printf("************\n");
+	//printf("************\n");
+	//printf("    *  *    \n");
+	//printf("    *  *    \n");
+
+	//printf("Name    Age    Gender\n");
+	//printf("---------------------\n");
+	//printf("Jack    18     man\n");
+
+	printf("I lost my cellphone!");
+
 	return 0;
+
 }
